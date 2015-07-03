7765f263391fcf16b3d8cc1868e6f88b6d3eed78
wbuczak <wojciech.buczak@gmail.com>
2015-07-03 14:18:06 +0200

    a bit of cleanup

--- src/main/java/org/ogn/gateway/live/LiveGlidernetForwarder.java
+++ src/main/java/org/ogn/gateway/live/LiveGlidernetForwarder.java
@@ -15,155 +15,140 @@ import org.ogn.commons.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-// import java.util.concurrent.locks.ReentrantLock;
-
-// import com.hazelcast.config.Config;
-// import com.hazelcast.core.Hazelcast;
-// import com.hazelcast.core.HazelcastInstance;
 
 /**
  * live.glidernet.org forwarder plug-in for OGN gateway
  * 
  * @author Seb, re-factoring: wbuczak
  */
-public class LiveGlidernetForwarder implements OgnAircraftBeaconForwarder, TimeWindowBufferListener {
+public class LiveGlidernetForwarder implements OgnAircraftBeaconForwarder,
+		TimeWindowBufferListener {
 
-    static {
-        System.setProperty("hazelcast.logging.type", "slf4j");
-    }
+	static {
+		System.setProperty("hazelcast.logging.type", "slf4j");
+	}
 
-    public static final String SERVICE_NAME = "live.glidernet.org";
-    public static final String VERSION = "1.0.0";
+	public static final String SERVICE_NAME = "live.glidernet.org";
+	public static final String VERSION = "1.0.0";
 
-    private static final Logger LOG = LoggerFactory.getLogger(LiveGlidernetForwarder.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(LiveGlidernetForwarder.class);
 
-    private static final int MAX_BUFFER_SIZE = 1500;
-    private static final int BUFFER_TIME_WINDOW = 2000;
+	private static final int MAX_BUFFER_SIZE = 1500;
+	private static final int BUFFER_TIME_WINDOW = 2000;
 
-    public static final String FIX_PREFIX = "fix[]=";
-    public static final char DELIMITER = ',';
-    public static final String AMPERSAND = "&";
+	public static final String FIX_PREFIX = "fix[]=";
+	public static final char DELIMITER = ',';
+	public static final String AMPERSAND = "&";
 
-    private TimeWindowBuffer<String> buffer;
+	private TimeWindowBuffer<String> buffer;
 
-    // private Lock lock;
+	private MsgSender forwarder;
 
-    private MsgSender forwarder;
+	private boolean initialized = false;
 
-    // private boolean clusterMode;
+	@Override
+	public String getName() {
+		return SERVICE_NAME + " forwarder";
+	}
 
-    private boolean initialized = false;
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
 
-    @Override
-    public String getName() {
-        return SERVICE_NAME + " forwarder";
-    }
+	@Override
+	public String getDescription() {
+		return "relays OGN aircraft beacons to " + SERVICE_NAME;
+	}
 
-    @Override
-    public String getVersion() {
-        return VERSION;
-    }
+	@Override
+	public void init() {
 
-    @Override
-    public String getDescription() {
-        return "relays OGN aircraft beacons to " + SERVICE_NAME;
-    }
+		if (initialized)
+			return;
 
-    @Override
-    public void init() {
+		buffer = new TimeWindowBuffer<>(MAX_BUFFER_SIZE, BUFFER_TIME_WINDOW,
+				this, AMPERSAND);
 
-        if (initialized)
-            return;
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"classpath:application-context.xml");
+		ctx.refresh();
+		String passwd = ctx.getBean(String.class, "passwd");
+		ctx.close();
 
-        buffer = new TimeWindowBuffer<>(MAX_BUFFER_SIZE, BUFFER_TIME_WINDOW, this, AMPERSAND);
+		if (forwarder == null)
+			forwarder = new HttpMsgSender(passwd);
 
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:application-context.xml");
-        ctx.refresh();
-        String passwd = ctx.getBean(String.class, "passwd");
-        // clusterMode = ctx.getBean(Boolean.class, "clusterMode");
-        ctx.close();
+		initialized = true;
+	}
 
-        // if (clusterMode) {
-        // Config config = new Config();
-        // HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
-        // lock = h.getLock("OgnGatewatLivePluginLock");
-        // } else {
-        // lock = new ReentrantLock();
-        // }
+	void init(MsgSender sender) {
+		if (!initialized) {
+			this.forwarder = sender;
+			init();
+		}
+	}
 
-        if (forwarder == null)
-            forwarder = new HttpMsgSender(passwd);
+	@Override
+	public void stop() {
+	}
 
-        initialized = true;
-    }
+	/**
+	 * default constructor
+	 */
+	public LiveGlidernetForwarder() {
 
-    void init(MsgSender sender) {
-        if (!initialized) {
-            this.forwarder = sender;
-            init();
-        }
-    }
+	}
 
-    @Override
-    public void stop() {
-        // lock.unlock();
-    }
+	static String beaconToStr(AircraftBeacon beacon,
+			AircraftDescriptor descriptor) {
+		StringBuilder bld = new StringBuilder(FIX_PREFIX);
 
-    /**
-     * default constructor
-     */
-    public LiveGlidernetForwarder() {
+		// if descriptor is provided take cn and reg number from it
+		if (descriptor != null && descriptor.isKnown()) {
+			bld.append(descriptor.getCN()).append(DELIMITER)
+					.append(descriptor.getRegNumber());
+		} else {
+			bld.append(
+					beacon.getAddress().substring(
+							beacon.getAddress().length() - 2))
+					.append(DELIMITER).append(beacon.getAddress());
+		}
 
-    }
+		bld.append(DELIMITER);
 
-    static String beaconToStr(AircraftBeacon beacon, AircraftDescriptor descriptor) {
-        StringBuilder bld = new StringBuilder(FIX_PREFIX);
+		bld.append(format("%.4f", beacon.getLat())).append(DELIMITER);
+		bld.append(format("%.4f", beacon.getLon())).append(DELIMITER);
+		bld.append(format("%.0f", beacon.getAlt())).append(DELIMITER);
+		bld.append(format("%d", beacon.getTimestamp() / 1000))
+				.append(DELIMITER);
+		bld.append(format("%d", beacon.getTrack())).append(DELIMITER);
+		bld.append(format("%.0f", beacon.getGroundSpeed())).append(DELIMITER);
+		bld.append(format("%.1f", beacon.getClimbRate())).append(DELIMITER);
+		bld.append(format("%d", beacon.getAircraftType().getCode())).append(
+				DELIMITER);
+		bld.append(beacon.getReceiverName()).append(DELIMITER);
+		bld.append(beacon.getId());
 
-        // if descriptor is provided take cn and reg number from it
-        if (descriptor != null && descriptor.isKnown()) {
-            bld.append(descriptor.getCN()).append(DELIMITER).append(descriptor.getRegNumber());
-        } else {
-            bld.append(beacon.getAddress().substring(beacon.getAddress().length() - 2)).append(DELIMITER)
-                    .append(beacon.getAddress());
-        }
+		return bld.toString();
+	}
 
-        bld.append(DELIMITER);
+	@Override
+	public void onBeacon(AircraftBeacon beacon, AircraftDescriptor descriptor,
+			String rawBeacon) {
 
-        bld.append(format("%.4f", beacon.getLat())).append(DELIMITER);
-        bld.append(format("%.4f", beacon.getLon())).append(DELIMITER);
-        bld.append(format("%.0f", beacon.getAlt())).append(DELIMITER);
-        bld.append(format("%d", beacon.getTimestamp() / 1000)).append(DELIMITER);
-        bld.append(format("%d", beacon.getTrack())).append(DELIMITER);
-        bld.append(format("%.0f", beacon.getGroundSpeed())).append(DELIMITER);
-        bld.append(format("%.1f", beacon.getClimbRate())).append(DELIMITER);
-        bld.append(format("%d", beacon.getAircraftType().getCode())).append(DELIMITER);
-        bld.append(beacon.getReceiverName()).append(DELIMITER);
-        bld.append(beacon.getId());
+		LOG.trace("sending beacon to {}: {} {}", SERVICE_NAME,
+				JsonUtils.toJson(beacon), JsonUtils.toJson(descriptor));
+		buffer.add(beaconToStr(beacon, descriptor));
+	}
 
-        return bld.toString();
-    }
-
-    @Override
-    public void onBeacon(AircraftBeacon beacon, AircraftDescriptor descriptor, String rawBeacon) {
-        // if (lock.tryLock()) {
-
-        // forward only beacons which have tracked flag ON
-        // if (descriptor.isTracked()) {
-        LOG.trace("sending beacon to {}: {} {}", SERVICE_NAME, JsonUtils.toJson(beacon), JsonUtils.toJson(descriptor));
-        buffer.add(beaconToStr(beacon, descriptor));
-        // }
-        // } else {
-        // LOG.info("discarding, another gateway instance (in the cluster) is forwarding beacon: {}",
-        // beacon.getRawPacket());
-        // }
-    }
-
-    /**
-     * TimeWindowBuffer's callback
-     */
-    @Override
-    public void tick(String msg, int elements) {
-        LOG.trace("tick received with {} elements", elements);
-        forwarder.send(msg);
-    }
+	/**
+	 * TimeWindowBuffer's callback
+	 */
+	@Override
+	public void tick(String msg, int elements) {
+		LOG.trace("tick received with {} elements", elements);
+		forwarder.send(msg);
+	}
 }
